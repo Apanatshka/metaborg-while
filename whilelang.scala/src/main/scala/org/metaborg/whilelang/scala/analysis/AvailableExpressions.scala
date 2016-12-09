118e5f3066dd8e5be26e31311e4c81aacade2663
Jeff Smits <jeff.smits@gmail.com>
2016-12-09 09:47:58 +0100

    Moved Classical Analysis helper to popa-experiments

--- whilelang.scala/src/main/scala/org/metaborg/whilelang/scala/analysis/AvailableExpressions.scala
+++ whilelang.scala/src/main/scala/org/metaborg/whilelang/scala/analysis/AvailableExpressions.scala
@@ -1,6 +1,6 @@
 package org.metaborg.whilelang.scala.analysis
 
-import org.metaborg.popa.mfp.{IntraControlFlow, SetLattice}
+import org.metaborg.popa.mfp.{ClassicalAnalysis, IntraControlFlow, SetLattice}
 import org.metaborg.scalaterms.Origin
 import org.metaborg.scalaterms.spoofax.{AnalysisResult, EditorMessage}
 import org.metaborg.whilelang.scala.ast.MExpr.SExpr
@@ -8,33 +8,38 @@ import org.metaborg.whilelang.scala.ast.MStatement.SLabeledStatement
 import org.metaborg.whilelang.scala.ast.MStatement.SLabeledStatement._
 import org.metaborg.whilelang.scala.ast.MWhilelang.SStart.Labeled1
 import org.metaborg.whilelang.scala.ast.Utils
-import org.strategoxt.lang.Context
 
 /**
-  * Classical analysis which is a forward, must analysis
+  * Classical forward+must analysis that collects the available (already calculated) expressions on each statement
   */
-object AvailableExpressions extends ClassicalAnalysis[Int, SExpr, Labeled1, SLabeledStatement] {
-  override def run(ast: Labeled1): AnalysisResult = {
+object AvailableExpressions extends ClassicalAnalysis {
+  override type Label = Int
+  override type Prop = SExpr
+  override type Start = Labeled1
+  override type RelevantNode = SLabeledStatement
+  override type Result = AnalysisResult
+
+  override def run(ast: Start): Result = {
     val analysisResult = analyze(ast)
-    val labelToOrigin: Map[Int, Origin] = Utils.labelToOriginMap(ast)
+    val labelToOrigin: Map[Label, Origin] = Utils.labelToOriginMap(ast)
     val notes: List[EditorMessage] = analysisResult.toList
       .map({ case (lbl, res) => EditorMessage.from(prepareProperties(res), labelToOrigin(lbl)) })
 
     AnalysisResult(ast.toSTerm, errors = List(), warnings = List(), notes)
   }
 
-  override def prepareProp(prop: SExpr): String = Utils.toString(prop)
+  override def prepareProp(prop: Prop): String = Utils.toString(prop)
 
-  override def labelMap(ast: Labeled1): Map[Int, SLabeledStatement] = Utils.labelToAstMap(ast)
+  override def labelMap(ast: Start): Map[Label, RelevantNode] = Utils.labelToAstMap(ast)
 
-  override def lattice(ast: Labeled1): SetLattice[SExpr] = SetLattice.mustAnalysis[SExpr](Utils.collectAExprs(ast))
+  override def lattice(ast: Start): SetLattice[Prop] = SetLattice.mustAnalysis[SExpr](Utils.collectAExprs(ast))
 
-  override def flow(ast: Labeled1): IntraControlFlow[Int] = Utils.flow(ast)
+  override def flow(ast: Start): IntraControlFlow[Label] = Utils.flow(ast)
 
-  override def extremalValue(ast: Labeled1): Set[SExpr] = Set.empty
+  override def extremalValue(ast: Start): Set[Prop] = Set.empty
 
-  override def kill(nodes: SLabeledStatement,
-                    property: Set[SExpr]): Set[SExpr] = nodes match {
+  override def kill(node: RelevantNode,
+                    property: Set[Prop]): Set[Prop] = node match {
     case LabeledAssign3(id1, expr2, int3) => property.filter(p => Utils.collectRefs(p).contains(id1.string))
     case LabeledSkip1(int1) => Set.empty
     case Seq2(labeledstatement1, labeledstatement2) => Set.empty
@@ -42,7 +47,7 @@ object AvailableExpressions extends ClassicalAnalysis[Int, SExpr, Labeled1, SLab
     case LabeledWhile3(expr1, int2, labeledstatement3) => Set.empty
   }
 
-  override def gen(nodes: SLabeledStatement): Set[SExpr] = nodes match {
+  override def gen(node: RelevantNode): Set[Prop] = node match {
     case LabeledAssign3(id1, expr2, int3) =>
       Utils.collectAExprs(expr2).filterNot(p => Utils.collectRefs(p).contains(id1.string))
     case LabeledSkip1(int1) => Set.empty
--- whilelang.scala/src/main/scala/org/metaborg/whilelang/scala/analysis/ReachingDefinitions.scala
+++ whilelang.scala/src/main/scala/org/metaborg/whilelang/scala/analysis/ReachingDefinitions.scala
@@ -1,49 +1,52 @@
 package org.metaborg.whilelang.scala.analysis
 
-import org.metaborg.popa.mfp.{IntraControlFlow, SetLattice}
+import org.metaborg.popa.mfp.{ClassicalAnalysis, IntraControlFlow, SetLattice}
 import org.metaborg.scalaterms.Origin
 import org.metaborg.scalaterms.spoofax.{AnalysisResult, EditorMessage}
 import org.metaborg.whilelang.scala.ast.MStatement.SLabeledStatement
 import org.metaborg.whilelang.scala.ast.MStatement.SLabeledStatement._
 import org.metaborg.whilelang.scala.ast.MWhilelang.SStart.Labeled1
-import org.metaborg.whilelang.scala.ast.Utils
-import org.metaborg.whilelang.scala.ast.SINTToInt
-import org.strategoxt.lang.Context
+import org.metaborg.whilelang.scala.ast.{SINTToInt, Utils}
 
 /**
-  * Classical analysis which is a forward, may analysis
+  * Classical forward+may analysis that collects the place of latest write to every variable on each statement
   */
-object ReachingDefinitions extends ClassicalAnalysis[Int, (String, Option[Int]), Labeled1, SLabeledStatement] {
-  override def prepareProp(prop: (String, Option[Int])): String = (prop._1, prop._2.map(_.toString).getOrElse("?"))
-    .toString
+object ReachingDefinitions extends ClassicalAnalysis {
+  override type Label = Int
+  override type Prop = (String, Option[Int])
+  override type Start = Labeled1
+  override type RelevantNode = SLabeledStatement
+  override type Result = AnalysisResult
 
-  override def labelMap(ast: Labeled1): Map[Int, SLabeledStatement] = Utils.labelToAstMap(ast)
+  override def prepareProp(prop: Prop): String =
+    (prop._1, prop._2.map(_.toString).getOrElse("?")).toString
 
-  override def lattice(ast: Labeled1): SetLattice[(String, Option[Int])] = {
+  override def labelMap(ast: Start): Map[Label, RelevantNode] = Utils.labelToAstMap(ast)
+
+  override def lattice(ast: Start): SetLattice[Prop] = {
     val refs = Utils.collectRefs(ast)
     val labels = Utils.labelToAstMap(ast).keySet
     val top = Utils.cartesianProduct(refs, labels.map(Some)).union(refs.map((_, None)))
     SetLattice.mayAnalysis(top)
   }
 
-  override def flow(ast: Labeled1): IntraControlFlow[Int] = Utils.flow(ast)
+  override def flow(ast: Start): IntraControlFlow[Label] = Utils.flow(ast)
 
-  override def extremalValue(ast: Labeled1): Set[(String, Option[Int])] = {
+  override def extremalValue(ast: Start): Set[Prop] = {
     val refs = Utils.collectRefs(ast)
     refs.map((_, None))
   }
 
-  override def run(ast: Labeled1): AnalysisResult = {
+  override def run(ast: Start): Result = {
     val analysisResult = analyze(ast)
-    val labelToOrigin: Map[Int, Origin] = Utils.labelToOriginMap(ast)
+    val labelToOrigin: Map[Label, Origin] = Utils.labelToOriginMap(ast)
     val notes: List[EditorMessage] = analysisResult.toList
       .map({ case (lbl, res) => EditorMessage.from(prepareProperties(res), labelToOrigin(lbl)) })
 
     AnalysisResult(ast.toSTerm, errors = List(), warnings = List(), notes)
   }
 
-  override def kill(nodes: SLabeledStatement,
-                    property: Set[(String, Option[Int])]): Set[(String, Option[Int])] = nodes match {
+  override def kill(nodes: RelevantNode, property: Set[Prop]): Set[Prop] = nodes match {
     case LabeledAssign3(id1, expr2, int3) => property.filter(p => id1.string == p._1)
     case LabeledSkip1(int1) => Set.empty
     case Seq2(labeledstatement1, labeledstatement2) => Set.empty
@@ -51,7 +54,7 @@ object ReachingDefinitions extends ClassicalAnalysis[Int, (String, Option[Int]),
     case LabeledWhile3(expr1, int2, labeledstatement3) => Set.empty
   }
 
-  override def gen(nodes: SLabeledStatement): Set[(String, Option[Int])] = nodes match {
+  override def gen(nodes: RelevantNode): Set[Prop] = nodes match {
     case LabeledAssign3(id1, expr2, int3) => Set((id1.string, Some(SINTToInt(int3))))
     case LabeledSkip1(int1) => Set.empty
     case Seq2(labeledstatement1, labeledstatement2) => Set.empty
