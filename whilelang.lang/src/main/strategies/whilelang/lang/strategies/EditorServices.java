2f2304078cf7c7f9798dd43a66e2e9b846bd320e
Jeff Smits <jeff.smits@gmail.com>
2017-02-06 16:02:24 +0100

    Baby steps.. control-flow.flo specifies control flow from this stratego code is generated to generate an adjacency list of the CFG

--- whilelang.lang/src/main/strategies/whilelang/lang/strategies/EditorServices.java
+++ whilelang.lang/src/main/strategies/whilelang/lang/strategies/EditorServices.java
@@ -6,30 +6,30 @@ import org.strategoxt.lang.Strategy;
 import org.metaborg.whilelang.scala.analysis.*;
 
 public class EditorServices {
-	public static final editor_analyze_0_0 editor_analyze_0_0 = new editor_analyze_0_0();
-	
-	static class editor_analyze_0_0 extends Strategy {
-		@Override
-		public IStrategoTerm invoke(Context context, IStrategoTerm current) {
-			return EditorServicesImpl$.MODULE$.editorAnalyze(context, current);
-		}
-	}
-	
-	public static final editor_hover_0_0 editor_hover_0_0 = new editor_hover_0_0();
-	
-	static class editor_hover_0_0 extends Strategy {
-		@Override
-		public IStrategoTerm invoke(Context context, IStrategoTerm current) {
-			return EditorServicesImpl$.MODULE$.editorHover(context, current);
-		}
-	}
-	
-	public static final editor_resolve_0_0 editor_resolve_0_0 = new editor_resolve_0_0();
-	
-	static class editor_resolve_0_0 extends Strategy {
-		@Override
-		public IStrategoTerm invoke(Context context, IStrategoTerm current) {
-			return EditorServicesImpl$.MODULE$.editorResolve(context, current);
-		}
-	}
+//	public static final editor_analyze_0_0 editor_analyze_0_0 = new editor_analyze_0_0();
+//	
+//	static class editor_analyze_0_0 extends Strategy {
+//		@Override
+//		public IStrategoTerm invoke(Context context, IStrategoTerm current) {
+//			return EditorServicesImpl$.MODULE$.editorAnalyze(context, current);
+//		}
+//	}
+//	
+//	public static final editor_hover_0_0 editor_hover_0_0 = new editor_hover_0_0();
+//	
+//	static class editor_hover_0_0 extends Strategy {
+//		@Override
+//		public IStrategoTerm invoke(Context context, IStrategoTerm current) {
+//			return EditorServicesImpl$.MODULE$.editorHover(context, current);
+//		}
+//	}
+//	
+//	public static final editor_resolve_0_0 editor_resolve_0_0 = new editor_resolve_0_0();
+//	
+//	static class editor_resolve_0_0 extends Strategy {
+//		@Override
+//		public IStrategoTerm invoke(Context context, IStrategoTerm current) {
+//			return EditorServicesImpl$.MODULE$.editorResolve(context, current);
+//		}
+//	}
 }
--- whilelang.lang/src/main/strategies/whilelang/lang/strategies/InteropRegisterer.java
+++ whilelang.lang/src/main/strategies/whilelang/lang/strategies/InteropRegisterer.java
@@ -6,9 +6,9 @@ import org.strategoxt.lang.Strategy;
 public class InteropRegisterer extends JavaInteropRegisterer {
 	public InteropRegisterer() {
 		super(new Strategy[] {
-				EditorServices.editor_analyze_0_0,
-				EditorServices.editor_hover_0_0,
-				EditorServices.editor_resolve_0_0
+//				EditorServices.editor_analyze_0_0,
+//				EditorServices.editor_hover_0_0,
+//				EditorServices.editor_resolve_0_0
 		});
 	}
 }
